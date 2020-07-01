26f80070fe9684fc09873a36054bda1acdaa9057
Rohmad <rohmad37@ymail.com>
2020-07-01 16:58:19 +0700

    Exercise 3

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import Table from './components/Table/Table';
+import Table from "./components/Table/Table";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-    const students = [
-        {
-            name: 'Yono',
-            age: 17,
-            address: 'Serang',
-            sex: 'male',
-        },
-        {
-            name: 'Icha',
-            age: 20,
-            address: 'Pekanbaru',
-            sex: 'female',
-        },
-        {
-            name: 'Ahmad',
-            age: 30,
-            address: 'Jakarta',
-            sex: 'male',
-        },
-        {
-            name: 'Lily',
-            age: 15,
-            address: 'Bandung',
-            sex: 'female',
-        },
-    ];
-    return (
-        <div className='App'>
-            <Table />
-        </div>
-    );
+  const students = [
+    {
+      name: "Yono",
+      age: 17,
+      address: "Serang",
+      sex: "male",
+    },
+    {
+      name: "Icha",
+      age: 20,
+      address: "Pekanbaru",
+      sex: "female",
+    },
+    {
+      name: "Ahmad",
+      age: 30,
+      address: "Jakarta",
+      sex: "male",
+    },
+    {
+      name: "Lily",
+      age: 15,
+      address: "Bandung",
+      sex: "female",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Table student={students} />
+    </div>
+  );
 }
 
 export default App;
